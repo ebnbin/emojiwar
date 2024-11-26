41810c27d170faa65e94a16742b1d7bcb2fb6e09
Bin Zhang <ebnbin@gmail.com>
2024-11-25 23:31:45 -0800

    Update AndroidLauncher

--- android/src/main/kotlin/dev/ebnbin/emojiwar/android/AndroidLauncher.kt
+++ android/src/main/kotlin/dev/ebnbin/emojiwar/android/AndroidLauncher.kt
@@ -1,18 +1,15 @@
 package dev.ebnbin.emojiwar.android
 
 import android.os.Bundle
-
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import dev.ebnbin.emojiwar.EmojiWar
 
-/** Launches the Android application. */
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initialize(EmojiWar(), AndroidApplicationConfiguration().apply {
-            // Configure your application here.
-            useImmersiveMode = true // Recommended, but not required.
-        })
+        val listener = EmojiWar()
+        val config = AndroidApplicationConfiguration()
+        initialize(listener, config)
     }
 }
