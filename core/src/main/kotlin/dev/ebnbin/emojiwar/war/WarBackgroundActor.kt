06a31f57e11fe736b0022140f418adb440e5167e
Bin Zhang <ebnbin@gmail.com>
2024-11-27 04:43:18 -0800

    Create WarBackgroundActor

---
+++ core/src/main/kotlin/dev/ebnbin/emojiwar/war/WarBackgroundActor.kt
@@ -0,0 +1,88 @@
+package dev.ebnbin.emojiwar.war
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.scenes.scene2d.Actor
+import kotlin.random.Random
+
+class WarBackgroundActor(
+    textureList: List<Texture>,
+    rows: Int,
+    columns: Int,
+    density: Float,
+    scaleMin: Float,
+    scaleMax: Float,
+    offsetAbs: Float,
+    private val alpha: Float,
+) : Actor() {
+    init {
+        setSize(columns.toFloat(), rows.toFloat())
+    }
+
+    private val shapeRenderer: ShapeRenderer = ShapeRenderer().also {
+        it.color = Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
+    }
+
+    private data class Tile(
+        val texture: Texture,
+        val x: Float,
+        val y: Float,
+        val scale: Float,
+        val rotation: Float,
+    )
+
+    private val tileList: List<Tile> = mutableListOf<Tile>().also { tileList ->
+        for (y in 0 until rows) {
+            for (x in 0 until columns) {
+                if (Random.nextFloat() > density) continue
+                val tile = Tile(
+                    texture = textureList.random(),
+                    x = x.toFloat() + Random.nextFloat() * offsetAbs * 2 - offsetAbs,
+                    y = y.toFloat() + Random.nextFloat() * offsetAbs * 2 - offsetAbs,
+                    scale = Random.nextFloat() * (scaleMax - scaleMin) + scaleMin,
+                    rotation = Random.nextFloat() * 360f,
+                )
+                tileList.add(tile)
+            }
+        }
+    }
+
+    override fun draw(batch: Batch, parentAlpha: Float) {
+        super.draw(batch, parentAlpha)
+        batch.end()
+        Gdx.gl.glEnable(GL20.GL_BLEND)
+        shapeRenderer.projectionMatrix = batch.projectionMatrix
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+        shapeRenderer.rect(x, y, width, height)
+        shapeRenderer.end()
+        Gdx.gl.glDisable(GL20.GL_BLEND)
+        batch.begin()
+        val oldColor = batch.color
+        batch.color = color.cpy().also { it.a *= parentAlpha * alpha }
+        tileList.forEach { tile ->
+            batch.draw(
+                tile.texture,
+                x + tile.x,
+                y + tile.y,
+                0.5f,
+                0.5f,
+                1f,
+                1f,
+                tile.scale,
+                tile.scale,
+                tile.rotation,
+                0,
+                0,
+                tile.texture.width,
+                tile.texture.height,
+                false,
+                true,
+            )
+        }
+        batch.color = oldColor
+    }
+}
--- core/src/main/kotlin/dev/ebnbin/emojiwar/war/WarStage.kt
+++ core/src/main/kotlin/dev/ebnbin/emojiwar/war/WarStage.kt
@@ -2,17 +2,12 @@ package dev.ebnbin.emojiwar.war
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import dev.ebnbin.emojiwar.emojiWar
 import dev.ebnbin.kgdx.scene.LifecycleStage
-import ktx.actors.alpha
 import kotlin.math.max
-import kotlin.random.Random
 
 class WarStage : LifecycleStage(object : ScreenViewport() {
     override fun update(screenWidth: Int, screenHeight: Int, centerCamera: Boolean) {
@@ -25,32 +20,18 @@ class WarStage : LifecycleStage(object : ScreenViewport() {
         camera.update()
     }
 
-    private val shapeRenderer: ShapeRenderer = ShapeRenderer().also {
-        it.color = Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
-    }
-
     init {
-        val emojiTextureList = emojiWar.emojiTextureMap.values.toList().subList(0, BACKGROUND_EMOJI_DIVERSITY)
-        for (y in 1 until ROWS - 1) {
-            for (x in 1 until COLUMNS - 1) {
-                if (Random.nextFloat() > BACKGROUND_EMOJI_DENSITY) continue
-                val texture = emojiTextureList.random()
-                Image(texture).also {
-                    it.setSize(1f, 1f)
-                    it.setPosition(x.toFloat(), y.toFloat())
-                    it.setOrigin(Align.center)
-                    it.scaleY = -1f
-                    it.alpha = BACKGROUND_EMOJI_ALPHA
-                    it.rotation = Random.nextFloat() * 360f
-                    it.setScale(Random.nextFloat() * (BACKGROUND_EMOJI_SCALE_MAX - BACKGROUND_EMOJI_SCALE_MIN) +
-                        BACKGROUND_EMOJI_SCALE_MIN)
-                    it.moveBy(
-                        Random.nextFloat() * BACKGROUND_EMOJI_OFFSET_ABS * 2 - BACKGROUND_EMOJI_OFFSET_ABS,
-                        Random.nextFloat() * BACKGROUND_EMOJI_OFFSET_ABS * 2 - BACKGROUND_EMOJI_OFFSET_ABS,
-                    )
-                    addActor(it)
-                }
-            }
+        WarBackgroundActor(
+            textureList = emojiWar.emojiTextureMap.values.toList().subList(0, BACKGROUND_EMOJI_DIVERSITY),
+            rows = ROWS,
+            columns = COLUMNS,
+            density = BACKGROUND_EMOJI_DENSITY,
+            scaleMin = BACKGROUND_EMOJI_SCALE_MIN,
+            scaleMax = BACKGROUND_EMOJI_SCALE_MAX,
+            offsetAbs = BACKGROUND_EMOJI_OFFSET_ABS,
+            alpha = BACKGROUND_EMOJI_ALPHA,
+        ).also {
+            addActor(it)
         }
     }
 
@@ -103,21 +84,6 @@ class WarStage : LifecycleStage(object : ScreenViewport() {
             0f,
         )
         camera.update()
-    }
-
-    override fun draw() {
-        Gdx.gl.glEnable(GL20.GL_BLEND)
-        shapeRenderer.projectionMatrix = camera.combined
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
-        shapeRenderer.rect(0f, 0f, COLUMNS.toFloat(), ROWS.toFloat())
-        shapeRenderer.end()
-        Gdx.gl.glDisable(GL20.GL_BLEND)
-        super.draw()
-    }
-
-    override fun dispose() {
-        shapeRenderer.dispose()
-        super.dispose()
     }
 
     companion object {
